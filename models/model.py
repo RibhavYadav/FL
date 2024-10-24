31b6ffaaf6d4bd4bced2d46073c798fad6c2e25d
draby2466 <ribhavyadav2466@gmail.com>
2024-10-25 00:56:24 +0530

    Base model template.

---
+++ models/model.py
@@ -0,0 +1,15 @@
+"""Base model template"""
+
+
+class Model:
+    def __init__(self, weights=None, bias=None, learning_rate=0.01, iterations=1000, verbose=False):
+        """Initialises the model"""
+        pass
+
+    def predict(self, x):
+        """Predicts output from current weights and bias"""
+        pass
+
+    def fit(self, x_train, y_train):
+        """Fits the model to the training data"""
+        pass
