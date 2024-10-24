c613444e98aa10833826afec37a3b01fabe75b71
draby2466 <ribhavyadav2466@gmail.com>
2024-10-25 01:35:34 +0530

    Added comments.

--- client-side.py
+++ client-side.py
@@ -1,21 +1,9 @@
 from models import model_template
 
-"""
-'Client' side implementation of the devices connected in Federated Learning
-Client class with functions for it's working:
-    - receive_update: receives the parameters from the global model along with other parameters
-        - weights: array like
-        - batch_size: minimum number of rows in a batch
-    - train_local: initialise model and train it for a fixed batch size
-        - initialise_model: start a local Linear Regression model
-        - split_data: split the data into the given number of batch size
-        - train_model: train the model on the local data for 1 batch
-    - send_local: sends the updated model parameters back to the global model
-"""
-
 
 class Client:
     def __init__(self, learning_model: model_template, weights=None, bias=None, batch_size=64):
+        # Initialises the client with the model, weights and bias from the global model.
         self.model = learning_model
         self.weights = weights
         self.bias = bias
@@ -23,9 +11,11 @@ class Client:
         self.batches_done = 0
 
     def receive_update(self, weights, bias):
+        # Updates the weights and bias of the local model.
         self.weights, self.bias = weights, bias
 
     def train_local(self, x_train, y_train):
+        # Trains the local model on a batch of data
         split = self.batches_done * self.batch_size
         x_batch = x_train[split:split + self.batch_size]
         y_batch = y_train[split:split + self.batch_size]
