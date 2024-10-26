f2a26c5ba5f2100f4600aa5cc92d65744cf4186e
draby2466 <ribhavyadav2466@gmail.com>
2024-10-26 17:19:42 +0530

    Updated init.

--- models/model_template.py
+++ models/model_template.py
@@ -1,9 +1,16 @@
+import tensorflow as tf
+
 """Base model template"""
 
 
 class Model:
     def __init__(self, weights=None, bias=None, learning_rate=0.01, iterations=1000, verbose=False):
         """Initialises the model"""
+        self.weights = tf.Variable(weights, dtype=tf.float32) if weights is not None else None
+        self.bias = tf.Variable(bias, dtype=tf.float32) if bias is not None else None
+        self.lr = learning_rate
+        self.iterations = iterations
+        self.verbose = verbose
         pass
 
     def predict(self, x):
