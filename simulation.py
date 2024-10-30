0e642b7ca911a001f36670a5cdec5a3ec1ec04ae
BSfett2213 <shivbhargava2004@gmail.com>
2024-10-30 09:57:25 +0530

    Graphs.

--- simulation.py
+++ simulation.py
@@ -5,6 +5,7 @@ from server_side import Server
 from models.LinearRegression import LinearRegression
 from models.OptimisedSGD import OptimisedSGD
 from models.UnoptimisedSGD import UnoptimisedSGD
+import seaborn as sns, matplotlib.pyplot as plt
 
 data = pd.read_csv("wine+quality/winequality-red.csv", delimiter=';')
 x = data.iloc[:, :-1].values
@@ -33,7 +34,7 @@ linear_loss = [linear_server.get_loss(y_test, x_test).numpy()]
 usgd_loss = [usgd_server.get_loss(y_test, x_test).numpy()]
 osgd_loss = [osgd_server.get_loss(y_test, x_test).numpy()]
 
-for i in range(5):
+for i in range(10):
     print(f"Epoch: {i}")
     linear_server.aggregate(), usgd_server.aggregate(), osgd_server.aggregate()
     linear_loss.append(linear_server.get_loss(y_test, x_test).numpy())
@@ -43,3 +44,13 @@ for i in range(5):
 print(linear_loss)
 print(usgd_loss)
 print(osgd_loss)
+
+titles = ["Linear Regression", "UnoptimisedSDG", "OptimisedSDG"]
+plt.figure(figsize=(10, 10))
+for i in range(3):
+    plt.subplot(3, 3, i + 1)
+    plt.plot(range(1, 11), linear_loss[i], cmap="yellow")
+    plt.title(titles[i])
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+plt.show()
